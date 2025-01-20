2fb77b30400888fb94a375d78c3b8d7b8e0df0b5
r3DBAD <americium77@gmail.com>
2025-01-20 22:35:42 +0300

    prototype

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-for i in range(10):
-    print("counter" + " " + str(i))
+import eel
+import tkinter as tk
+
+eel.init('UI')
+
+root = tk.Tk()
+
+screen_width = root.winfo_screenwidth()  
+screen_height = root.winfo_screenheight() 
+
+w = 1280
+h = 720
+
+x_pos = (screen_width - w) // 2
+y_pos = (screen_height - h) // 2
+root.attributes('-fullscreen', True)
+eel.start('main.html', size=(w, h), position=(x_pos, y_pos))
