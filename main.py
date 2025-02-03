3ab6f24e536109774c9eb7e435c21a941d9fcb3e
r3DBAD <americium77@gmail.com>
2025-02-03 18:58:53 +0300

    try pyqt

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
-import eel
-import tkinter as tk
+import requests
+import matplotlib
+import sys
+from PyQt6.QtCore import QSize,Qt
+from PyQt6.QtWidgets import QApplication,QWidget,QMainWindow,QPushButton,QLabel
 
-eel.init('UI')
-
-root = tk.Tk()
-
-screen_width = root.winfo_screenwidth()  
-screen_height = root.winfo_screenheight() 
-
-w = 1280
-h = 720
-
-x_pos = (screen_width - w) // 2
-y_pos = (screen_height - h) // 2
-root.attributes('-fullscreen', True)
-eel.start('main.html', size=(w, h), position=(x_pos, y_pos))
+class WeatherApp(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setFixedSize(QSize(1280,720))
+        self.setWindowTitle('Weather moon')
+        header = QLabel('Выберите город')
+app = QApplication(sys.argv)
+wind = WeatherApp()
+wind.show()
+app.exec()
